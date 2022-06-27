b0f9d9fff6ebac789d967fcbd5a93382cd75e570
Inna Groysman <33602729+Innush@users.noreply.github.com>
2022-06-27 19:38:18 -0400

    google login btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useEffect } from "react";
 import './App.css';
 
 function App() {
+
+  // @ts-ignore
+  function handleCallbackResponse( response ) {
+    console.log(" Encoded JWT ID token " + response.credential);
+  }
+
+  useEffect(() => {
+    /* global google */
+
+    // @ts-ignore
+    google.accounts.id.initialize({
+      client_id: "993513948523-anaep7s1tmm4n0db313bruanu26qehfr.apps.googleusercontent.com" ,
+      callback: handleCallbackResponse
+    });
+
+    // @ts-ignore
+    google.accounts.id.renderButton(
+        document.getElementById("signInDiv"),
+        { theme: "outline" , size: "large" }
+    );
+
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id = "signInDiv"> </div>
+
     </div>
   );
 }
