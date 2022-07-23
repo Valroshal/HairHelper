4a378ea850fc3877288d8624ebd433aac1dd7285
Innush <innasol90@gmail.com>
2022-07-23 16:43:56 -0400

    added useState for email and password (onChange) new function existingUser (onClick Log In)

--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import {useState, useEffect, useCallback} from 'react';
 import { GoogleLoginButton } from 'ts-react-google-login-component';
 
 
 const Login = () => {
+    //GOOGLE BTN
     const preLoginTracking = () => {
         console.log('Attemp to login with google');
     }
@@ -10,7 +12,6 @@ const Login = () => {
         // handle error if login got failed...
         console.error(error)
     }
-
     const responseGoogle = (googleUser: gapi.auth2.GoogleUser) => {
         const id_token = googleUser.getAuthResponse(true).id_token
         const googleId = googleUser.getId()
@@ -20,25 +21,47 @@ const Login = () => {
         // Make user login in your system
         // login success tracking...
     }
-
     const clientConfig = { client_id: '993513948523-anaep7s1tmm4n0db313bruanu26qehfr.apps.googleusercontent.com' }
+    //END GOOGLE BTN
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    //
+    // useEffect(() => {
+    //     console.log("use effect my var" , email );
+    // },[email]);
 
-            return (
-                <div className="loginForm">
-                    <h1 className="text-orange-600">LOGIN</h1>
-                    <input className="" type="text" placeholder="Username or email..."/>
-                    <input type="password" placeholder="Password..."/>
-                    <button>Sign in</button>
+    const existingUser = useCallback(() => {
+        const user = {
+            email: email,
+            password: password
+        };
+        console.log("user is :" , user);
+    },[email,password]);
+    return (
+        <div className="loginForm">
+            <h1 className="text-orange-600">LOGIN</h1>
+            <input className="" type="text"
+                   placeholder="Username or email..."
+                   onChange={(e) => {setEmail(e.target.value)}}
+                   value={email}
+            />
+            <input type="password"
+                   placeholder="Password..."
+                   onChange={(e) => {setPassword(e.target.value)}}
+                   value={password}
 
-                    <GoogleLoginButton
-                        responseHandler={responseGoogle}
-                        clientConfig={clientConfig}
-                        preLogin={preLoginTracking}
-                        failureHandler={errorHandler}
-                    />
-                </div>
-            );
+            />
+            <button>Sign in</button>
+            <button onClick={existingUser}>Log in</button>
+            <GoogleLoginButton
+                responseHandler={responseGoogle}
+                clientConfig={clientConfig}
+                preLogin={preLoginTracking}
+                failureHandler={errorHandler}
+            />
+        </div>
+    );
 }
 
 export default Login
